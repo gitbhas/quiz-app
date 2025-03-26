c98019e653981ed1221bad27a33c6952272ab4cf
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-03-26 01:03:26 -0400

    Update aws-exports-template.js

--- aws-exports-template.js
+++ aws-exports-template.js
@@ -1,9 +1,9 @@
 // Rename this file to aws-exports.js after filling in your values
 const awsmobile = {
-    "aws_project_region": "YOUR_REGION",  // e.g., "us-east-1"
-    "aws_cognito_region": "YOUR_REGION",  // e.g., "us-east-1"
-    "aws_user_pools_id": "YOUR_USER_POOL_ID",  // e.g., "us-east-1_xxxxxxxxx"
-    "aws_user_pools_web_client_id": "YOUR_APP_CLIENT_ID",  // e.g., "1234567890abcdefghijklmno"
+    "aws_project_region": "us-east-1",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_8FyE7Hy4o",
+    "aws_user_pools_web_client_id": "m4js7ltsb49qcp2mmb42835ir",
     "oauth": {}
 };
 
