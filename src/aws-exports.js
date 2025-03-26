1c33b188fab9a6a0b4525874cc9145e349a246a3
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-03-26 01:19:19 -0400

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_8FyE7Hy4o",
+    "aws_user_pools_web_client_id": "m4js7ltsb49qcp2mmb42835ir",
+    "oauth": {}
+};
+
+export default awsmobile;
